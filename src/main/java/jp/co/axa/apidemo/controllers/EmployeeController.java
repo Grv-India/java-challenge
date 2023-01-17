45f53433683052a457b5bdc0296e28f89ad23f8a
grvgupta <grvgupta65@gmail.com>
2023-01-17 15:02:44 +0900

    Swagger configured and docs updated at various locations

--- src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -1,5 +1,6 @@
 package jp.co.axa.apidemo.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,28 +19,38 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    @ApiOperation("Welcome route")
+    @GetMapping
+    public String welcome(){
+        return "Welcome to Employee Record System";
+    }
+    @ApiOperation("Fetch all employee records")
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
         List<Employee> employees = employeeService.retrieveEmployees();
         return employees;
     }
 
+    @ApiOperation("Fetch the employee record on the basis of passed unique id")
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable(name="employeeId")Long employeeId) {
         return employeeService.getEmployee(employeeId);
     }
 
+    @ApiOperation("Add the passed employee data to records")
     @PostMapping("/employees")
     public void saveEmployee(@RequestBody Employee employee){
         employeeService.saveEmployee(employee);
         System.out.println("Employee Saved Successfully");
     }
 
+    @ApiOperation("Delete employee data from records based on passed unique id")
     @DeleteMapping("/employees/{employeeId}")
     public String deleteEmployee(@PathVariable(name="employeeId")Long employeeId){
         return employeeService.deleteEmployee(employeeId);
     }
 
+    @ApiOperation("Do the changes in existing employee record")
     @PutMapping("/employees/{employeeId}")
     public Employee updateEmployee(@RequestBody Employee employee,
                                @PathVariable(name="employeeId")Long employeeId){
